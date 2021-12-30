f5da947dc35c998d9907b9816781c67df88ffb5a
Mambou-01 <cmambou06@gmail.com>
2021-12-30 07:34:47 +0100

    add useEffect and list pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState,useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mockPokemons';
  
 const App: FunctionComponent = () => {
- const [pokemons] = useState<Pokemon[]>(POKEMONS); 
+ const [pokemons,setPokemons] = useState<Pokemon[]>([]); 
+
+ useEffect(() =>{
+    setPokemons(POKEMONS);
+ },[]);
  
  return (
   <div>
-   <h1>Pokédex</h1>
-   <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+     <h1 className='text-center'>Pokédex</h1>
+     <div className="container">
+        <div className="row">
+           {pokemons.map(({id, name, picture, created}) =>(
+              <div className="col-md-8 mx-auto" key={id} >
+                 <div className="card mb-3">
+                    <div className="row ">
+                           <div className="col-md-4">
+                              <img src={picture} alt={name} className="img-fluid rounded-start" />
+                           </div>
+                           <div className="col-md-8">
+                              <div className="card-body">
+                                 <h5 className="card-title">{name}</h5>
+                                 <p className="card-text"><small> {created.toString()} </small></p>
+                              </div>
+                           </div>
+                    </div>
+                    
+                 </div>
+              </div>
+           ) )}
+        </div>
+     </div>
   </div>
   );
 }
